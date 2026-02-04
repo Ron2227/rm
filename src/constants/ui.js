965ebc212cfcdb495a9fbb2558a2e9936c752f93
Hemanth Bandlamudi <88618983+Ron2227@users.noreply.github.com>
2026-02-04 14:50:51 +0530

    ui.js

---
+++ src/constants/ui.js
@@ -0,0 +1,14 @@
+export const FONT_CLASS = 'font-sans';
+export const STORAGE_KEY = 'resume-maker-local-draft-v1';
+
+export const UI = {
+  BG_PRIMARY: 'bg-[#191919]',
+  BG_SECONDARY: 'bg-[#202020]',
+  TEXT_PRIMARY: 'text-[#EDEDED]',
+  TEXT_SECONDARY: 'text-[#B5B5B5]',
+  BORDER: 'border-[#2A2A2A]',
+  INPUT_BG: 'bg-[#191919]',
+  BUTTON_PRIMARY_BG: 'bg-[#FACC15]',
+  BUTTON_PRIMARY_HOVER: 'hover:bg-[#EAB308]',
+  BUTTON_TEXT_CONTRAST: 'text-black font-bold',
+};
