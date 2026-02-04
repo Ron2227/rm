bd7f0e98d852d992632a594e62f835f4f94262b9
Hemanth Bandlamudi <88618983+Ron2227@users.noreply.github.com>
2026-02-04 14:55:08 +0530

    SectionViewerMap.js

---
+++ src/components/viewer/SectionViewerMap.js
@@ -0,0 +1,10 @@
+export const SectionViewerMap = {
+  summary: SummaryViewer,
+  skills: SkillsViewer,
+  experience: ExperienceViewer,
+  education: EducationViewer,
+  certifications: CertificationsViewer,
+  languages: LanguagesViewer,
+  awards: AwardsViewer,
+  custom: CustomViewer,
+};
