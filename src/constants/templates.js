e168cc084aaadcec78027448543cfa8f56554ca1
Hemanth Bandlamudi <88618983+Ron2227@users.noreply.github.com>
2026-02-04 14:52:22 +0530

    templates.js

---
+++ src/constants/templates.js
@@ -0,0 +1,7 @@
+export const templates = {
+  classic: { name: 'Classic', layout: 'single' },
+  modern: { name: 'Modern', layout: 'two-column', sidebarBg: 'bg-white' },
+  bold: { name: 'Bold', layout: 'single' },
+  professional: { name: 'Professional', layout: 'two-column', sidebarBg: 'bg-gray-100' },
+  executive: { name: 'Executive', layout: 'single' },
+};
